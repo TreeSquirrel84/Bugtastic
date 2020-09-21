6073bb2cdd970788acb08ae2800fa47882b2bdf8
Warren D Rose II <Treesquirrel84@gmail.com>
2020-09-21 11:39:32 -0700

    Update main.cpp
    Finalizing error correction.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main()
   string name;
   char last_initial;
 
-  cout << "Hi. What is your first name? "
+  cout << "Hi. What is your first name? ";
   cin >> name;
-  cout << name <<, "what is the first letter of your last name? ";
+  cout << name << "what is the first letter of your last name? ";
   cin >> last_initial;
   cout << "Thanks, " << name << " " << last_initial<<"."<<endl;
   cout<<"Please also tell me how old you are: ";
@@ -20,10 +20,10 @@ int main()
   {
     cout<<"Hey kid, how do you like school?\n";
   }
-  if( age < 18 )
+  if( age < 18 ) {
     cout<<"Cool!"<<endl;
     cout<<"How's highschool going?\n";
-  else( age >= 18 );
+  } else( age >= 18 );
   {
     cout<<"Pleased to meet you!\n";
   }
